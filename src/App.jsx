e9777dbb76d1eb6c6c826f7290e680d79dd2f599
TatiYas <iasinovska_lawyer@ukr.net>
2024-07-03 21:47:01 +0200

    Update_FriendList_

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import PersonProfile from './components/PersonProfile/PersonProfile';
 import userData from './components/Data/userData.json'
+import FriendList from './components/FriendList'
+import friends from './components/FriendList'
 export default App;
 function App() {
   return (
-    
-      <PersonProfile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
+    <>
+    <PersonProfile
+    name={userData.username}
+    tag={userData.tag}
+    location={userData.location}
+    image={userData.avatar}
+    stats={userData.stats}
+  />
+  <FriendList friends={friends} />
+    </>
      
   );
 }
