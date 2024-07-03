be1010da2a91fe6a7fc512d6326ad734a653d655
TatiYas <iasinovska_lawyer@ukr.net>
2024-07-03 21:21:31 +0200

    Update_Person_

---
+++ src/components/PersonProfile/PersonProfile.jsx
@@ -0,0 +1,31 @@
+import style from './Profile.module.css';
+export default function PersonProfile({ name, tag, location, image, stats }) {
+  const { followers, views, likes } = stats;
+  return (
+    <div className={style.card}>
+      <div className={style.user}>
+        <img className={style.avatar} src={image} alt="User avatar" />
+        <p className={style.username}>{name}</p>
+        <p className={style.userTag}>{tag}</p>
+        <p className={style.userLocation}>{location}</p>
+      </div>
+
+      <ul className={style.list}>
+        <li className={style.listItem}>
+          <span>Followers</span>
+          <span className={style.listStat}>{followers}</span>
+        </li>
+
+        <li className={style.listItem}>
+          <span>Views</span>
+          <span className={style.listStat}>{views}</span>
+        </li>
+
+        <li className={style.listItem}>
+          <span>Likes</span>
+          <span className={style.listStat}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
